2552ce8b961900a98731ab26c47456fb56b83976
Yumi Takuma <yumitakuma@icloud.com>
2025-05-08 20:10:39 +0800

    Add bmc

--- src/components/TaskManager.js
+++ src/components/TaskManager.js
@@ -5,6 +5,7 @@ import DeletePrompt from "./DeletePrompt";
 import TaskList from "./TaskList";
 import { useDispatch, useSelector } from "react-redux";
 import { updateWWidth } from "../redux/taskSlice";
+import BMCIcon from "../assets/bmc-button.svg";
 
 const TaskManager = () => {
   const dispatch = useDispatch();
@@ -21,7 +22,7 @@ const TaskManager = () => {
 
   return (
     <>
-      <div className="pt-16 pb-20 px-5 overflow-y-auto lg:px-10 h-full w-full flex flex-col max-w-screen-xl mx-auto min-w-92.5 min-h-138">
+      <div className="py-14 px-5 overflow-y-auto lg:px-10 h-full w-full flex flex-col max-w-screen-xl mx-auto min-w-92.5 min-h-138">
         <div className="relative inline-block mx-auto text-center text-5xl font-light mb-10 font-playwright">
           <h1 className="text-indigo">taskite</h1>
           <span className="absolute -top-0.5 -left-0.5 text-yellow">
@@ -39,6 +40,34 @@ const TaskManager = () => {
             ))}
           </div>
         </DndProvider>
+        <div className="relative w-full mt-10 text-black/80 text-base flex flex-col gap-5 sm:flex-row justify-between items-center">
+          <p>
+            &copy; 2025&nbsp;
+            <a
+              className="underline"
+              href="https://takumayumi.pages.dev"
+              title="takumayumi"
+              target="_blank"
+              rel="noreferrer"
+            >
+              takumayumi
+            </a>
+          </p>
+          <a
+            href="https://buymeacoffee.com/yumicchi"
+            title="Buy me a hot chocolate"
+            target="_blank"
+            rel="noreferrer"
+            aria-label="Buy me a hot chocolate"
+          >
+            <img
+              src={BMCIcon}
+              alt="Buy me a hot chocolate"
+              width="179"
+              height="50"
+            />
+          </a>
+        </div>
       </div>
       <DeletePrompt />
     </>
